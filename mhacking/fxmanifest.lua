beb69bfc55a8e74db23fb0d795cf6efdb57cca8b
washerebeck-maker <washerebeck@gmail.com>
2026-08-26 05:15:53 -0500

    Add files via upload

---
+++ mhacking/fxmanifest.lua
@@ -0,0 +1,22 @@
+shared_script "@ReaperV4/imports/bypass.lua"
+shared_script "@ReaperV4/imports/bypass_s.lua"
+shared_script "@ReaperV4/imports/bypass_c.lua"
+lua54 "yes" -- needed for Reaper
+
+fx_version 'cerulean'
+game 'gta5'
+ui_page 'hack.html'
+client_scripts {
+  'mhacking.lua',
+  'sequentialhack.lua'
+}
+files {
+  'phone.png',
+  'snd/beep.ogg',
+  'snd/correct.ogg',
+  'snd/fail.ogg', 
+  'snd/start.ogg',
+  'snd/finish.ogg',
+  'snd/wrong.ogg',
+  'hack.html'
+}
---
+++ milostacos/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 'yes'
+dependencies {
+    '/server:4700'
+}
+dependency '/assetpacks'
