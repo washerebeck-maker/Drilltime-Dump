643a558b9db800845d24fc2a019f8e0b990aa4d3
washerebeck-maker <washerebeck@gmail.com>
2026-08-26 05:01:17 -0500

    Add files via upload

---
+++ 2strkbeng/__resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+    'audioconfig/2strkbeng_amp.dat10.rel',
+	'audioconfig/2strkbeng_game.dat151.rel',
+	'audioconfig/2strkbeng_sounds.dat54.rel',
+	'sfx/dlc_2strkbeng/2strkbeng.awc',
+	'sfx/dlc_2strkbeng/2strkbeng_npc.awc',
+}
+
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/2strkbeng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/2strkbeng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/2strkbeng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_2strkbeng'
+
+
