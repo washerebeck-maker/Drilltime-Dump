5850a2e3f5be5172b898e07b5f9c04a570803e67
washerebeck-maker <washerebeck@gmail.com>
2026-08-26 05:02:16 -0500

    Add files via upload

---
+++ 3dme/fxmanifest.lua
@@ -0,0 +1,13 @@
+shared_script "@ReaperV4/imports/bypass.lua"
+shared_script "@ReaperV4/imports/bypass_s.lua"
+shared_script "@ReaperV4/imports/bypass_c.lua"
+lua54 "yes" -- needed for Reaper
+
+fx_version 'cerulean'
+game 'gta5'
+author 'Elio'
+description '/me command but it\'s 3D printed'
+version '3.0'
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
