3f30206b5640457cb383569a38f005a67920fc87
washerebeck-maker <washerebeck@gmail.com>
2026-08-26 05:02:41 -0500

    Add files via upload

---
+++ 1370atosmalepackv7/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with Durty Cloth Tool v3.2.0.5 (https://gta.clothing)
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_1370atosmalepackv7_shop.meta',
+  'pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_1370atosmalepackv7_shop.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
---
+++ 1370malev7_5/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Generated with Durty Cloth Tool v3.2.0.5 (https://gta.clothing)
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+author '1370 Mods'
+files {
+  'mp_m_freemode_01_mp_m_1370malev7_5_shop.meta',
+  'pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_1370malev7_5_shop.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
