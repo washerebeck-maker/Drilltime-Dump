186ad7aefae5a66d92830e593a941839a86a53b1
washerebeck-maker <washerebeck@gmail.com>
2026-08-26 05:02:27 -0500

    Add files via upload

---
+++ 911b/__resource.lua
@@ -0,0 +1,42 @@
+shared_script "@ReaperV4/imports/bypass.lua"
+shared_script "@ReaperV4/imports/bypass_s.lua"
+shared_script "@ReaperV4/imports/bypass_c.lua"
+lua54 "yes" -- needed for Reaper
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+
+	'data/**/carcols.meta',
+	'data/**/carvariations.meta',
+	'data/**/handling.meta',
+	'data/**/vehiclelayouts.meta',
+	'data/**/vehicles.meta',
+
+  
+
+	'audioconfig/tagt3flat6_game.dat151.rel',
+	'audioconfig/tagt3flat6_sounds.dat54.rel',
+	'sfx/dlc_tagt3flat6/tagt3flat6.awc',
+	'sfx/dlc_tagt3flat6/tagt3flat6_npc.awc'
+	
+
+
+}
+
+	data_file 'VEHICLE_LAYOUTS_FILE'	'data/**/vehiclelayouts.meta'
+	data_file 'HANDLING_FILE'			'data/**/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE'	'data/**/vehicles.meta'
+	data_file 'CARCOLS_FILE'			'data/**/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE'	'data/**/carvariations.meta'
+
+
+
+	data_file 'AUDIO_GAMEDATA' 'audioconfig/tagt3flat6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tagt3flat6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tagt3flat6'
+	
+
+
+
+	client_script 'veh_label.lua'
